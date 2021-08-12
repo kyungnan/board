cf05ec49f91658c246249a5e31d6dd76b71c6113
kyungnan <baekn1023@google.com>
2021-08-12 13:57:40 +0900

    make home

---
+++ src/main/java/BoardExample/board/domain/BoardMember.java
@@ -0,0 +1,11 @@
+package BoardExample.board.domain;
+
+import lombok.Data;
+
+@Data
+public class BoardMember {
+    private String username;        //아이디
+    private String password;        //패스워드
+    private String name;            //사용자이름
+    private String email;           //사용자이메일
+}
---
+++ src/main/resources/db/db.sql
@@ -0,0 +1,24 @@
+--DB 생성
+CREATE DATABASE board_project default CHARACTER SET UTF8;
+
+--board 테이블 생성
+CREATE TABLE board(
+	postno INT Primary KEY AUTO_INCREMENT,
+	subject VARCHAR(50) NOT NULL,
+	content VARCHAR(4000),
+	writer VARCHAR(32) NOT NULL,
+	regdate DATE NOT NULL,
+	modidate DATE NOT NULL,
+	count INT DEFAULT 0
+);
+
+--board_member 테이블 생성
+CREATE TABLE `board_member` (
+	`id` BIGINT NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR(50) NOT NULL,
+	`password` VARCHAR(100) NOT NULL,
+	`name` VARCHAR(50) NOT NULL,
+	`email` VARCHAR(50) NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE INDEX `username` (`username`)
+)
