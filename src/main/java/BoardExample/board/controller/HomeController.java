101daba649011f8c80b2c21f770a7868d76dab5c
kyungnan <baekn1023@google.com>
2021-08-28 11:56:02 +0900

    update 210828

--- src/main/java/BoardExample/board/controller/HomeController.java
+++ src/main/java/BoardExample/board/controller/HomeController.java
@@ -1,14 +1,13 @@
 package BoardExample.board.controller;
 
+import BoardExample.board.config.auth.PrincipalDetails;
 import BoardExample.board.mapper.BoardMemberMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,10 +16,9 @@ public class HomeController {
     private final BoardMemberMapper boardMemberMapper;
 
     @GetMapping("/")
-    public String home(Authentication authentication, Model model) {
-        if (authentication != null){
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            String nameUserDetails = boardMemberMapper.getNameById(userDetails.getUsername());
+    public String home(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
+        if (principalDetails != null){
+            String nameUserDetails = boardMemberMapper.getNameById(principalDetails.getUsername());
             model.addAttribute("nameUserDetails", nameUserDetails);
         }
         return "home";
