fb5f3afcbebc58b687c421c9e12837fffcd448d1
kyungnan <baekn1023@google.com>
2021-09-21 14:45:58 +0900

    add join validation

--- src/main/java/BoardExample/board/controller/AccountController.java
+++ src/main/java/BoardExample/board/controller/AccountController.java
@@ -7,9 +7,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/members")
@@ -31,14 +39,24 @@ public class AccountController {
     }
 
     @PostMapping("/join")
-    public String join(BoardMember member, Model model){
-        log.info("## join ::: " +  member + " ##");
-        int result = boardMemberService.join(member);
+    public String join(@Valid BoardMember member, Errors errors, Model model){
 
+        if(errors.hasErrors()) {
+            log.debug("===hasErrors===" + errors.hasErrors());
+            log.debug("===getFieldErrors===" + errors.getFieldErrors());
+
+            for(FieldError value : errors.getFieldErrors()) {
+                log.debug("===defaultMessage===" + value.getDefaultMessage());
+                model.addAttribute("valid_"+value.getField(), value.getDefaultMessage());
+            }
+            return "members/join";
+        }
+
+        int result = boardMemberService.join(member);
         if (result == -1){
             String msg = "해당 ID는 이미 사용중입니다.";
             model.addAttribute("msg", msg);
-            return "/members/join";
+            return "members/join";
         }
         return "redirect:/members/login";
     }
--- src/main/java/BoardExample/board/domain/BoardMember.java
+++ src/main/java/BoardExample/board/domain/BoardMember.java
@@ -3,17 +3,33 @@ package BoardExample.board.domain;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @Data
 @NoArgsConstructor
 public class BoardMember {
     private int id;                 //회원번호
-    private String username;        //아이디
-    private String password;        //패스워드
-    private String name;            //사용자이름
-    private String email;           //사용자이메일
-    private String role;            //사용자권한
 
+    @NotBlank(message = "ID는 필수 입력값 입니다.")
+    @Length(min = 5, max = 20, message = "ID는 5자 이상 20자 이하로 입력해주세요.")
+    private String username;        //아이디
+
+    @NotBlank(message = "PASSWORD는 필수 입력값 입니다.")
+    @Length(min = 8, max = 20, message = "PASSWORD는 8자 이상 20자 이하로 입력해주세요.")
+    private String password;        //패스워드
+
+    @NotBlank(message = "Name은 필수 입력값 입니다.")
+    private String name;            //사용자이름
+
+    @NotBlank(message = "Email은 필수 입력값 입니다.")
+    @Email(message = "Email 형식이 아닙니다.")
+    private String email;           //사용자이메일
+
+    private String role;            //사용자권한
     private String provider;        //google
     private String providerId;      //google_sub
 
